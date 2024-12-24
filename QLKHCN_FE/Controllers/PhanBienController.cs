6ff606b61261419bb0de94a5c57d13fb1783c454
NKDuy <nkduy.dev@gmail.com>
2024-12-24 07:56:36 +0700

    chore(design): add PhanBien layout + pages

---
+++ QLKHCN_FE/Controllers/PhanBienController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace QLKHCN_FE.Controllers
+{
+    public class PhanBienController : BaseController
+    {
+        public ActionResult Index()
+        {
+            if (!IsAuthenticated)
+            {
+                Session["ReturnUrl"] = Request.Url.AbsoluteUri;
+                return RedirectToAction("Login", "NguoiDung");
+            }
+
+            return View();
+        }
+
+        public ActionResult BienTapChuyenMon()
+        {
+            if (!IsAuthenticated)
+            {
+                Session["ReturnUrl"] = Request.Url.AbsoluteUri;
+                return RedirectToAction("Login", "NguoiDung");
+            }
+
+            return View();
+        }
+    }
+}
