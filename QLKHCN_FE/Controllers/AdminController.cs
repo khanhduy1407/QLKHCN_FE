257777dae162a3920a9106fdc33d6f107ffaa956
NKDuy <nkduy.dev@gmail.com>
2025-01-19 17:15:13 +0700

    feat(QA): added q and a in admin

---
+++ QLKHCN_FE/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace QLKHCN_FE.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: CauHoi
+        public ActionResult CauHoi()
+        {
+            return View();
+        }
+    }
+}
